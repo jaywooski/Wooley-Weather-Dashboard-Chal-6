158bc24cbea3e843ef9e1296bd84b47b1b4fac80
Woogster <john.m.wooley@gmail.com>
2021-11-22 17:40:22 -0600

    Added code/refactored code to look better

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// Initialize data query selectors
 let cityForm = document.querySelector("#city-form");
 let cityContainerEl = document.querySelector("#city-container");
 let cityInput = document.querySelector("#city-input");
@@ -6,6 +7,9 @@ let cityTitle = document.querySelector("#current-city");
 let currentWeatherEl = document.querySelector(".current")
 let dailyForecastEl = document.querySelector("#forecast-display");
 let forecastHeader = document.querySelector("#forecast-header");
+let forecastDate = document.querySelector(".forecast-time");
+
+
 // Personal API key to be used for project and from here on out
 
 const apiKey = "cc742ab3f18c60ff03116b342797094a"
@@ -14,15 +18,14 @@ const apiKey = "cc742ab3f18c60ff03116b342797094a"
 
 
 
- function getCityWeather(cityName) {
-    // event.preventDefault();
-    // console.log(cityForm.value);
+function getCityWeather(cityName) {
+
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
     
     console.log(cityName);
     console.log(apiUrl);
 
-    // console.log(cityForm);
+    
     fetch(apiUrl)
     .then(function(response) {
         if (response.ok) {
@@ -54,6 +57,8 @@ const apiKey = "cc742ab3f18c60ff03116b342797094a"
                                     uvIndexDiv.setAttribute("class", "extreme");
                                 };
                                 
+                                // Data array of faily weather attributes
+                                // Index ranging from 1-5 for next five days in data object
                                 const dailyData = [
                                     data2.daily[1],
                                     data2.daily[2],
@@ -61,17 +66,22 @@ const apiKey = "cc742ab3f18c60ff03116b342797094a"
                                     data2.daily[4],
                                     data2.daily[5]
                                 ];
+
                                 uvIndexDiv.innerHTML = `UV Index: ${data2.current.uvi}`
                                 topDivContent2.appendChild(uvIndexDiv);
 
                                 // 5 day forecast list creation
-                                dailyData.forEach(displayForecast); 
-                                
+                                dailyData.forEach(displayForecast);
                             })
-                        }// Currently: ${data.weather[0].main}
-                    }).catch(function(error) {
-                        alert("Error: City name invalid");
+                        }
+
                     })
+                    .catch(function(error) {
+
+                        alert("Error: Unable to Connect to OpenWeatherMap");
+
+                    })
+
                 console.log(data);
                 
                 let forecastDiv = document.createElement("div");
@@ -99,17 +109,14 @@ const apiKey = "cc742ab3f18c60ff03116b342797094a"
                     <div>Lo Temp: ${Math.floor(data.main.temp_min)} F</div>
                     <div>Humidity: ${data.main.humidity}%</div>
                     <div>Wind: ${Math.floor(data.wind.speed)}mph</div>`;
-                
-                
-                
             })
-        }// Currently: ${data.weather[0].main}
+        }
     })
     .catch(function(error) {
-        alert("Error: City name invalid");
+        alert("Error: Unable to Connect to OpenWeatherMap");
     })
 } 
-// cityButton.addEventListener("submit", getCityName);
+
 
 var searchCity = function(event) {
 
@@ -128,7 +135,9 @@ var searchCity = function(event) {
 
     }
     else {
-        alert("Unable to connect to OpenWeatherMap")
+        alert("Invalid City Name: Try Again!")
+        forecastHeader.innerHTML = "";
+        cityTitle.innerHTML = "";
     }
 
     
@@ -138,20 +147,54 @@ var searchCity = function(event) {
 
 var displayForecast = function(array){
     // initialized variables as daily weather attributes
-    // let icon =  `${array.weather.icon}`;
-    // let iconInfo = `${array.weather.description}`;
     let hiTemp = `${Math.floor(array.temp.max)}`;
     let loTemp = `${Math.floor(array.temp.min)}`;
     let windSpeed = `${Math.floor(array.wind_speed)}`;
     let humidity = `${array.humidity}`;
+    let date = `${array.dt}`;
+    // converts time [Inspired by StackOverflow! :) ]
+        function timeConverter(UNIX_timestamp){
+            var a = new Date(UNIX_timestamp * 1000); // Create new DATE object and used its built in methods below
+            var months = [
+                'Jan',
+                'Feb',
+                'Mar',
+                'Apr',
+                'May',
+                'Jun',
+                'Jul',
+                'Aug',
+                'Sep',
+                'Oct',
+                'Nov',
+                'Dec'
+            ];
+            var year = a.getFullYear();
+            var month = months[a.getMonth()];
+            var date = a.getDate();
+            
+            var time = date + ' ' + month + ' ' + year;
+            return time;
+        }
+      console.log(timeConverter(date));
+   
 
     // append each list as a child to ul under class ".forecast"
+    let dateHeader = document.createElement("h4"); // starting with header for time to display
+    dateHeader.setAttribute("class", "forecast-time"); // set class to "forecast-time" to manipulate
+    dateHeader.innerHTML = `${timeConverter(date)}`; // set inner html to sorted date
+
     let bigDiv = document.createElement("li");
     bigDiv.setAttribute("class", "border-2 p-1");
 
     let forecastTopDiv = document.createElement("div");
     forecastTopDiv.setAttribute("class", "flex-col justify-center border-b-1 p-1");
-    forecastTopDiv.innerHTML = `<div class=" flex justify-center"><img src="http://openweathermap.org/img/wn/${array.weather[0].icon}@2x.png" alt="${array.weather[0].description}"></div>`;
+    forecastTopDiv.innerHTML = 
+    `<div class="flex justify-center">
+    <img src="http://openweathermap.org/img/wn/${array.weather[0].icon}@2x.png" alt="${array.weather[0].description}">
+    </div>`;
+
+    forecastTopDiv.append(dateHeader);
 
     // div elements to append to bigDiv
     let forecastTopDivContent = document.createElement("div");
@@ -173,8 +216,8 @@ var displayForecast = function(array){
     dailyForecastEl.appendChild(bigDiv);
     
 }
-//  searchCity();
 
+// Event Listener(s)
 cityForm.addEventListener("submit", searchCity);
 
 
