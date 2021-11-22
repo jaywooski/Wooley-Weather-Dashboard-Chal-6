de3c844696f72a4851897d3072963aac3bbe04be
Woogster <john.m.wooley@gmail.com>
2021-11-22 04:20:09 -0600

    Added function ability to clear fields after searching

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,11 @@
-let searchForm = document.querySelector("#search-form");;
+let cityForm = document.querySelector("#city-form");
+let cityContainerEl = document.querySelector("#city-container");
 let cityInput = document.querySelector("#city-input");
 let cityButton = document.querySelector("#city-button");
 let cityTitle = document.querySelector("#current-city");
 let currentWeatherEl = document.querySelector(".current")
-let dailyForecastEl = document.querySelector(".forecast");
+let dailyForecastEl = document.querySelector("#forecast-display");
+let forecastHeader = document.querySelector("#forecast-header");
 // Personal API key to be used for project and from here on out
 
 const apiKey = "cc742ab3f18c60ff03116b342797094a"
@@ -74,18 +76,17 @@ const apiKey = "cc742ab3f18c60ff03116b342797094a"
                 
                 let forecastDiv = document.createElement("div");
                 let topDiv = document.createElement("div");
-                let bottomDiv = document.createElement("div");
                 let topDivContent = document.createElement("div");
                 let topDivContent2 = document.createElement("div");
                 
                 cityTitle.textContent = data.name; // adds title of city
+                forecastHeader.innerHTML = `5 Day Forecast for ${data.name}`;
                 currentWeatherEl.appendChild(forecastDiv);
                 // Adds styling class and creates divs for display
                 forecastDiv.appendChild(topDiv);
                 topDiv.setAttribute("class", "flex justify-center ")
                 topDivContent.setAttribute("class", "flex-initial");
 
-                forecastDiv.appendChild(bottomDiv);
                 topDivContent2.setAttribute("class", "flex-initial flex-col pt-3 pb-3")
                 topDiv.appendChild(topDivContent);
                 topDiv.appendChild(topDivContent2);
@@ -93,11 +94,11 @@ const apiKey = "cc742ab3f18c60ff03116b342797094a"
                 `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="${data.weather[0].description}">`;
 
                 topDivContent2.innerHTML = 
-                    `<div>Currently: ${data.weather[0].main} / ${data.main.temp} F</div>
-                    <div>Hi Temp: ${data.main.temp_max} F</div>
-                    <div>Lo Temp: ${data.main.temp_min} F</div>
+                    `<div>Currently: ${data.weather[0].main} / ${Math.floor(data.main.temp)} F</div>
+                    <div>Hi Temp: ${Math.floor(data.main.temp_max)} F</div>
+                    <div>Lo Temp: ${Math.floor(data.main.temp_min)} F</div>
                     <div>Humidity: ${data.main.humidity}%</div>
-                    <div>Wind Speed: ${data.wind.speed} mph</div>`;
+                    <div>Wind: ${Math.floor(data.wind.speed)}mph</div>`;
                 
                 
                 
@@ -114,39 +115,66 @@ var searchCity = function(event) {
 
     // prevents page from refreshing by default
     event.preventDefault();
+    dailyForecastEl.innerHTML = "";
+    currentWeatherEl.innerHTML = "";
     
     // get value from input element
     cityName = cityInput.value.trim()
     
     if (cityName) {
         getCityWeather(cityName);
+        // clear old content
+        cityInput.value = "";
+
     }
     else {
         alert("Unable to connect to OpenWeatherMap")
     }
 
-    // clear old content
-    cityInput.textContent = "";
+    
     
     
 }
 
 var displayForecast = function(array){
-    // initialized variables as 
-    let icon = `<img src="http://openweathermap.org/img/wn/${array.weather.icon}@2x.png" alt="${array.weather.description}">`;
-    let hiTemp = `${array.temp.max}`;
-    let loTemp = `${array.temp.min}`;
-    let windSpeed = `${array.windspeed}`;
+    // initialized variables as daily weather attributes
+    // let icon =  `${array.weather.icon}`;
+    // let iconInfo = `${array.weather.description}`;
+    let hiTemp = `${Math.floor(array.temp.max)}`;
+    let loTemp = `${Math.floor(array.temp.min)}`;
+    let windSpeed = `${Math.floor(array.wind_speed)}`;
     let humidity = `${array.humidity}`;
 
+    // append each list as a child to ul under class ".forecast"
+    let bigDiv = document.createElement("li");
+    bigDiv.setAttribute("class", "border-2 p-1");
 
-    console.log(icon);
+    let forecastTopDiv = document.createElement("div");
+    forecastTopDiv.setAttribute("class", "flex-col justify-center border-b-1 p-1");
+    forecastTopDiv.innerHTML = `<div class=" flex justify-center"><img src="http://openweathermap.org/img/wn/${array.weather[0].icon}@2x.png" alt="${array.weather[0].description}"></div>`;
+
+    // div elements to append to bigDiv
+    let forecastTopDivContent = document.createElement("div");
+    forecastTopDivContent.innerHTML = `Hi: ${hiTemp}`;
+    forecastTopDiv.appendChild(forecastTopDivContent);
+
+    let forecastTopDivContent2 = document.createElement("div");
+    forecastTopDivContent2.innerHTML = `Lo: ${loTemp}`;
+    forecastTopDiv.appendChild(forecastTopDivContent2);
+
+
+    let forecastBottomDiv = document.createElement("div");
+    forecastBottomDiv.setAttribute("class", "p-1");
+    forecastBottomDiv.innerHTML = `<div>Wind: ${windSpeed}mph</div>` + `<div>Humidity: ${humidity}%</div>`;
+
+    bigDiv.append(forecastTopDiv, forecastBottomDiv);
     
-    
+
+    dailyForecastEl.appendChild(bigDiv);
     
 }
 //  searchCity();
 
-searchForm.addEventListener("submit", searchCity);
+cityForm.addEventListener("submit", searchCity);
 
 
